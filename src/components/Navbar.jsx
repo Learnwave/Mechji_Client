616c65b0c5c21d39a9077c18d80c65fe0789d34d
Learnwave <146640293+Learnwave@users.noreply.github.com>
2024-12-22 18:42:01 +0530

    user profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -78,6 +78,7 @@ const Navbar = () => {
 
         {userData ? <div className=' p-2 w-10 flex justify-center items-center rounded-full bg-black text-white relative group '> {userData.name[0].toUpperCase()} <div className=' absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10' > 
             <ul className='list-none m-0 p-2 bg-gray-100 text-sm '> 
+            <button>View Profiles</button>
             {!userData.isAccountVerified && <li onClick={sendVerificationOtp} className=' py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify email</li>}    
             
             <li  className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10' onClick={logout} >Logout</li>
